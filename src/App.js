d11bf7c79eef0e6f87e046af8d5f2af4213a05e7
Ben Flynn <flynn0087@gmail.com>
2021-03-03 13:09:43 -0600

    compenent mount function added to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //dependencies 
-import React from "react";
+import React, { Component } from "react";
 import Col from "./components/Col";
 import Container from "./components/Container";
 import EmployeeCard from "./components/EmployeeCard";
@@ -7,5 +7,29 @@ import SearchForm from "./components/SearchForm";
 import API from "./utils/API";
 import './App.css';
 
+// this section sets the state
+class App extends Component {
+   state = { employees: [], search: "" };
+
+  //get the data for the employees via the API
+  componentDidMount() {
+    API.search().then((res) => {
+      this.setState({
+        employees: res.data.results.map((employee, k) => ({
+          picture: employee.picture.large,
+          firstName: employee.name.first,
+          lastName: employee.name.last,
+          email: employee.email,
+          phone: employee.phone,
+          city: employee.location.city,
+          key: k,
+        })),
+      });
+    })
+    .catch((err) => console.log(err));
+  };
+
+  
+}
 
 export default App;
